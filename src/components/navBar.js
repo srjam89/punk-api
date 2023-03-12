03da1f97f931435bdd81119d816045b3a0ac243b
srjam89 <79203332+srjam89@users.noreply.github.com>
2023-03-12 12:14:34 +0000

    dom routing and nav bar added

---
+++ src/components/navBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import navCSS from "../styles/nav.module.css";
+
+const NavBar = () => {
+  return (
+    <nav className={navCSS.navbar}>
+      <img
+        className={navCSS.img}
+        src="/beer-animation-PhotoRoom (1).png"
+        alt=""
+      />
+      <ul className={navCSS.navLinks}>
+        <li>
+          <NavLink end to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/browsebeers">Browse Beers</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
